2de3d2c32c6ce0276439f8277e041e4e7e62e912
Fabian Bernat <fabbernat@gmail.com>
2024-12-08 15:07:21 +0100

    Let's gooo

--- Program.cs
+++ Program.cs
@@ -1,28 +1,43 @@
 using Microsoft.EntityFrameworkCore;
 using PetAsp.Data;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddDbContext<PetContext>(options =>
-    options.UseSqlite("Data Source=pets.db"));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseExceptionHandler("/Home/Error");
+    /**
+     * Jó helyre írom a IServiceCollection.AddControllers-t?
+     * VS2022 says: Please add all the required services by calling 'IServiceCollection.AddControllers' inside the call to 'ConfigureServices(...)' in the application startup code.'
+    */
+
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Adds services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddDbContext<PetContext>(options =>
+            options.UseSqlite("Data Source=pets.db"));
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+
+    }
 }
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
